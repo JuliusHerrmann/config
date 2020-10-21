ef15d38c47f8ab566f01054a5837e042a62f2687
JuliusHerrmann <herrmann.julius@gmx.de>
2020-10-21 23:31:53 +0200

    sink update for volume

--- .config/dwm/dwmbar.sh
+++ .config/dwm/dwmbar.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 function status (){
 	add_spaces "25" "$(printf "%s   %s " "$(date +%H:%M)" "$(date +%d.%m.%y)")"
-	#volume
-	if [ "$(pactl list sinks | grep "Mute:" | tail -c 2)" = "o" ]; then
+	#volumeq
+	#if [ "$(pactl list sinks | grep "Mute:" | head -2 | tail -1 | tail -c 2)" = "o" ]; then
+		#add_spaces "9" "$(pactl list sinks | grep '^[[:space:]]Volume:' | \
+			#head -n $(( $SINK + 2 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,') "
+	#else
+		#add_spaces "9" "--- "
+	#fi
+	SINK="$(pactl list sinks short|grep `pactl info|grep "Default Sink: "| sed 's/.*: //g'`|head -c 1)"
+	if [ "$(pactl list sinks | grep "Mute:" | head -2 | tail -1 | tail -c 2)" = "o" ]; then
 		add_spaces "9" "$(pactl list sinks | grep '^[[:space:]]Volume:' | \
 			head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,') "
 	else
