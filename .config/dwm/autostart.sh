fb33f715740c96e0ce5e0901f46dbaa75009f0e9
JuliusHerrmann <herrmann.julius@gmx.de>
2021-01-29 01:04:25 +0100

    added neo dwm autostart

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -3,8 +3,9 @@ xrandr --output DP-0 --mode 3440x1440 -r 99.98
 sxhkd -c .config/sxhkd/sxhkdrc_dwm &
 udiskie &
 batsignal &
-picom &
+picom --experimental-backends &
 caffeine &
+#caffeine-indicator &
 ./.fehbg &
 #./.config/polybar/launch.sh &
 #Screen lock
@@ -18,8 +19,12 @@ xinput --set-prop "ELAN1300:00 04F3:3057 Touchpad" "libinput Tapping Enabled" 1
 xinput --set-prop "ELAN1300:00 04F3:3057 Touchpad" "libinput Natural Scrolling Enabled" 1
 xinput --set-prop "ELAN1300:00 04F3:3057 Touchpad" "Coordinate Transformation Matrix" 18, 0, 0, 0, 18, 0, 0, 0, 1
 
-setxkbmap -option caps:escape &
+#setxkbmap de neo
 xset r rate 300 40 &
+#For normal de layout
+setxkbmap -option caps:escape &
+#For neo2 layout
+#xcape -e 'ISO_Level3_Shift=Escape' -t 200
 
 #Start the bar
 ./.config/dwm/dwmbar.sh &
