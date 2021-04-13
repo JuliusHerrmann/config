bc86d139b625ae7ee17487292ebaa8b67bd4f5d1
JuliusHerrmann <herrmann.julius@gmx.de>
2021-04-13 18:22:45 +0200

    set default keyboard to neo qwertz

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -20,11 +20,16 @@ xinput --set-prop "ELAN1300:00 04F3:3057 Touchpad" "libinput Natural Scrolling E
 xinput --set-prop "ELAN1300:00 04F3:3057 Touchpad" "Coordinate Transformation Matrix" 18, 0, 0, 0, 18, 0, 0, 0, 1
 
 #setxkbmap de neo
-xset r rate 300 40 &
+#xset r rate 300 40 &
 #For normal de layout
-setxkbmap -option caps:escape &
+#setxkbmap -option caps:escape &
 #For neo2 layout
 #xcape -e 'ISO_Level3_Shift=Escape' -t 200
+
+#set layout to neo_qwertz
+setxkbmap de neo_qwertz
+setxkbmap -option
+xcape -e 'ISO_Level3_Shift=Escape' -t 200
 
 #Start the bar
 ./.config/dwm/dwmbar.sh &
